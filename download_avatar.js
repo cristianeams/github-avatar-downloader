d945380acbea48bf46bc2ad7978f3e883b5306d1
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-12 00:46:49 +0000

    Prints a list of avatar Urls

--- download_avatar.js
+++ download_avatar.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var secret = require('secret');
+var secret = require('./secret');
 
 //console.log("Welcome to the GitHub Avatar Downloader");
 
@@ -7,16 +7,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body);
+    cb(err, data);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  //Loops through each item in the result array
+  for(var item of result){
+    console.log('Avatar Url:', item.avatar_url);
+  }
 });
